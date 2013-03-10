48f3f5c485febb9bef0c0c98f7cdd2244da078c4
Ruben Vincenten <rubenvincenten@gmail.com>
2013-03-10 14:29:10 +0100

    - update toc to 50200 - check the speechbubbles every 1/5 second instead of after every event (some bubbles were not picked up) - remove the font shadow (transparent at all times) - make the add-on global so people can hook into it.

--- code.lua
+++ code.lua
@@ -1,4 +1,4 @@
-local thisAddon = ...
+local thisAddon, ns = ...
 local select = select
 local pairs = pairs
 local GetCVarBool = GetCVarBool
@@ -10,62 +10,65 @@ local isBubble = function(frame)
 	return region:GetTexture() == [[Interface\Tooltips\ChatBubble-Background]]
 end
 
-local skinBubbles
-skinBubbles = function(frame, ...)
-	if not frame.isTransparentBubble and isBubble(frame) then 
-		for i=1, frame:GetNumRegions() do
-			local region = select(i, frame:GetRegions())
-			if region:GetObjectType() == "Texture" then
-				region:SetTexture(nil)
-			elseif region:GetObjectType() == "FontString" then
-				local f, s = region:GetFont()
-				region:SetFont(f, s, 'OUTLINE')
-			end
+local skinBubble = function(frame)
+	for i=1, frame:GetNumRegions() do
+		local region = select(i, frame:GetRegions())
+		if region:GetObjectType() == "Texture" then
+			region:SetTexture(nil)
+		elseif region:GetObjectType() == "FontString" then
+			local f, s = region:GetFont()
+			region:SetFont(f, s, "OUTLINE")
+			region:SetShadowColor(0, 0, 0, 0)
 		end
-		frame.isTransparentBubble =true
-	end
-	if (...) then 
-		skinBubbles(...)
 	end
 end
 
-local f = CreateFrame'Frame'
+local skinBubbles = function(...)
+	for i = 1, select('#', ...) do
+		local frame = select(i, ...)
+	
+		if not frame.isTransparentBubble and isBubble(frame) then 
+			skinBubble(frame)
+			frame.isTransparentBubble = true
+		end
+	end
+end
+
 local numChildren = -1
 local WorldFrame = WorldFrame
+
+local f = CreateFrame("Frame")
+f.elapsed = -2 -- wait two seconds
+f:SetScript("OnUpdate", function(self, elapsed)
+	self.elapsed = self.elapsed + elapsed
+	if self.elapsed < 0.2 then return end
+	self.elapsed = 0
+
+	local count = WorldFrame:GetNumChildren()
+	if count ~= numChildren then
+		numChildren = count
+		skinBubbles(WorldFrame:GetChildren())
+	end
+end)
+
+-- immediately trigger an update when a chat bubble is shown for the following events
+-- if the blizzard options has those speechbubbles enabled
 local events = {
 	CHAT_MSG_SAY = "chatBubbles", CHAT_MSG_YELL = "chatBubbles",
 	CHAT_MSG_PARTY = "chatBubblesParty", CHAT_MSG_PARTY_LEADER = "chatBubblesParty",
 	CHAT_MSG_MONSTER_SAY = "chatBubbles", CHAT_MSG_MONSTER_YELL = "chatBubbles", CHAT_MSG_MONSTER_PARTY = "chatBubblesParty",
 }
-
-f:SetScript('OnEvent', function(self, event, addon)
-	if addon ~= thisAddon then return end
-	self:UnregisterEvent'ADDON_LOADED'
-	self:Show()
-	self:SetScript('OnEvent', function(self, event)
-		if GetCVarBool(events[event]) then
-			local count = WorldFrame:GetNumChildren()
-			if(count ~= numChildren) then
-				numChildren = count
-				self.elapsed = 0
-				if not self:IsShown() then
-					self:Show()
-				end
-			end
-		end
-	end)
-	for k, v in pairs(events) do
-		self:RegisterEvent(k)
+f:SetScript("OnEvent", function(self, event)
+	if GetCVarBool(events[event]) then
+		self.elapsed = 0.158
 	end
 end)
-f:RegisterEvent'ADDON_LOADED'
+for k, v in pairs(events) do
+	f:RegisterEvent(k)
+end
 
-f:Hide()
-f.elapsed = 0
-f:SetScript('OnUpdate', function(self, elapsed)
-	self.elapsed = self.elapsed + elapsed
-	if self.elapsed > 0.1 then
-		self:Hide()
-		skinBubbles(WorldFrame:GetChildren())
-	end
-end)
+-- make the addon global.
+ns.isBubble = isBubble
+ns.skinBubble = skinBubble
+ns.updateFrame = f
+_G[thisAddon] = ns
